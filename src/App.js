af31fdb90447b7887a65b4e6b6745cc4e128d940
lgramajo99 <lucianongramajo@gmail.com>
2023-01-18 16:26:32 -0300

    Se completa los integracion requeridos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import './App.css'
 import Cards from './components/Cards.jsx'
 import Nav from './components/Nav'
 import About from './components/About'
 import Detail from './components/Detail'
 import Error from './components/Error';
+import Form from './components/Form';
 
 function App() {
   const [characters, setCharacters] = useState([]);
@@ -17,16 +18,35 @@ function App() {
         else window.alert('No hay personajes con ese ID')
       })
   }
+  const location = useLocation();
 
   const onClose = (id) => { setCharacters(characters.filter(character => character.id !== id)) }
 
+
+  const navigate = useNavigate();
+  const [access, setAccess] = useState(false);
+  const username = 'ejemplo@gmail.com';
+  const password = '1password';
+
+  function login(userData) {
+    if (userData.password === password && userData.username === username) {
+      setAccess(true);
+      navigate('/home');
+    }
+  }
+
+  useEffect(() => {
+    !access && navigate('/');
+  }, []);
+
   return (
     <div className='App'>
-      <Nav onSearch={onSearch} />
+      {location.pathname !== '/' && <Nav onSearch={onSearch} />}
       <Routes>
         <Route path='/home' caseSensitive={true} index={true} element={<Cards onClose={onClose} characters={characters} />} />
         <Route path={'/detail/:detailId'} element={<Detail />} />
         <Route path='/about' caseSensitive={true} element={<About />} />
+        <Route path='/' element={<Form login={login} />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </div >
