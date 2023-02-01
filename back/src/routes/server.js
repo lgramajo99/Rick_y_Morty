3e3000f2714e6aa32123079a3caf97cbd7879ecd
lgramajo99 <lucianongramajo@gmail.com>
2023-02-01 10:54:51 -0300

    peticion de character con el onSearch listo

--- back/src/routes/server.js
+++ back/src/routes/server.js
@@ -4,9 +4,8 @@ const characters = require('../utils/data.js')
 const _PORT = 3001;
 
 http.createServer((req, res) => {
-
+    res.setHeader('Access-Control-Allow-Origin', '*')
     if (req.url.includes('rickandmorty/character')) {
-        // console.log(`Servidor leventado en el puerto ${_PORT}`)
         let id = req.url.split('/').at(-1);
 
         // let characterFilter = characters.filter(ch => ch.id === Number(id))
